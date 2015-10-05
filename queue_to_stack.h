197b362b62f316bb0f7729f4d5d8dfd5ddfb7308
Creator-m <mjf2019@126.com>
2015-10-05 21:33:02 +0800

    Create queue_to_stack.h

---
+++ queue_to_stack.h
@@ -0,0 +1,45 @@
+#ifndef _LIST_QUEUE_H_
+#define _LIST_QUEUE_H_
+
+
+
+typedef int ELEMTYPE;
+
+
+typedef struct _list_queue
+{
+	ELEMTYPE  elem;
+	struct _list_queue *next;
+}list_queue;
+
+
+
+typedef struct _Listnode
+{
+	list_queue *head;
+	list_queue *tail;
+}node;
+
+
+
+bool init_list_queue(node   *q);
+
+bool is_empty(node  *q);
+
+bool push_list_queue(node *q,ELEMTYPE e);
+
+bool pop_list_queue(node *q,ELEMTYPE *e);
+
+bool destroy(node *q);
+
+bool get_head_queue(node *q,ELEMTYPE *e);
+
+bool clear(node *q);
+
+int my_get_length(node *q);
+
+
+
+static list_queue * buy_node();
+
+#endif _LIST_QUEUE_H_
